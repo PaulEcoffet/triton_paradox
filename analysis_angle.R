00cb7a3b189c71449f3f05ea3ed94f873d288ba3
Paul Ecoffet <ecoffet.paul@gmail.com>
2016-06-10 18:05:27 +0200

    Ajout de stats avec comptages par première note

--- analysis_angle.R
+++ analysis_angle.R
@@ -70,15 +70,13 @@ for (d in c(5, 6, 7)) {
   
   ### PLOTTING ###
   # +1/1000 because circ.plot bugs if angles are at a 0 value
-  circ.plot(cur$angles+1/1000, stack=TRUE, bins=360, shrink=5.5, main=paste("horloge pour", d))
+  circ.plot(cur$angles+1/1000, stack=TRUE, bins=360, shrink=1.5, main=paste("horloge pour", d, '-- heared rising'))
   arrows(0, 0, rbar*cos(mean_angle), rbar*sin(mean_angle))
   abline(0, -cos(mean_angle)/sin(mean_angle))
   text(0.9*cos(angles_i), 0.9*sin(angles_i), notes_name)
   dev.print(png, width=800, file=paste("/tmp/graph_", d, ".png",sep=""));
 
 }
-
-print("lol")
 
 trit_mean = aggregate(heared_raising ~ note1, data[data$dist == 6,], mean)
 plot(trit_mean, type="l", ylim=c(-0.1, 1.1))
@@ -100,3 +98,71 @@ for (i in 0:5) {
 }
 
 print(paste("frontiere raising is at", notes_name[best+1], "-", notes_name[best+7]))
+
+
+############################################################################################
+
+clock_plot = function (angles, titre="coucou") {
+    mean_angle = circ.mean(angles)
+    circ.plot(angles+1/1000, stack=TRUE, bins=360, shrink=1.5, main=titre)
+    arrows(0, 0, rbar*cos(mean_angle), rbar*sin(mean_angle))
+    abline(0, -cos(mean_angle)/sin(mean_angle))
+    text(0.9*cos(angles_i), 0.9*sin(angles_i), notes_name)
+    dev.print(png, width=800, file=paste("/tmp/graph_", titre, ".png",sep=""));
+    
+}
+
+rm(angles)
+
+cat("Test de l'influence de la préférence avec les quintes montantes")
+
+sub = data[data$dist==6, ]
+angles_m = c()
+angles_d = c()
+for (ind in 1:nrow(sub)){
+  note = sub$note1[ind]
+  if (sub$heared_raising[ind]) {
+    angles_m = c(angles_m, angles_i[note + 1])
+  } else {
+    angles_d = c(angles_d, angles_i[note + 1])
+  }
+    
+}
+
+clock_plot(angles_m, "horloge triton entendu montant en fonction de première note")
+text(0.9*cos(angles_i), 0.9*sin(angles_i), notes_name)
+
+sub = data[data$dist==7, ]
+angles_7 = c()
+for (ind in 1:nrow(sub)){
+  if (sub$heared_raising[ind]) {
+    note = sub$note1[ind]
+    angles_7 = c(angles_7, angles_i[note + 1])
+  }
+}
+clock_plot(angles_7, "horloge quinte entendu montant en fonction de première note")
+text(0.9*cos(angles_i), 0.9*sin(angles_i), notes_name)
+circ.mean(angles_7)
+circ.disp(angles_7)
+r.test(angles_7)
+rao.homogeneity(list(angles_m, angles_7), 0.05)
+
+cat("Test de l'influence de la préférence avec les quartes descendantes")
+
+clock_plot(angles_d, "horloge triton entendu descendant en fonction de première note")
+text(0.9*cos(angles_i), 0.9*sin(angles_i), notes_name)
+
+sub = data[data$dist==5, ]
+angles_5 = c()
+for (ind in 1:nrow(sub)){
+  if (!sub$heared_raising[ind]) {
+    note = sub$note1[ind]
+    angles_5 = c(angles_5, angles_i[note + 1])
+  }
+}
+clock_plot(angles_5, "horloge quarte entendu descendant en fonction de première note")
+text(0.9*cos(angles_i), 0.9*sin(angles_i), notes_name)
+circ.mean(angles_5)
+circ.disp(angles_5)
+r.test(angles_5)
+rao.homogeneity(list(angles_d, angles_5), 0.05)
